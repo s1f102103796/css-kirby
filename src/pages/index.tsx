ff1efd91ef76a729f16975140afe70187c88f5b8
s1f102103796 <85663261+s1f102103796@users.noreply.github.com>
2023-10-15 20:14:27 +0900

    kirbyが動く

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,28 @@ import styles from './index.module.css';
 const Home = () => {
   return (
     <div className={styles.basePoint}>
-      {/* <div className={styles.point} /> */}
-      <div className={styles.handRight} />
-      <div className={styles.handLeft} />
-      <div className={styles.footRight} />
-      <div className={styles.footLeft} />
-      <div className={styles.circleBody}>
-        <div className={styles.mouth} />
-        <div className={styles.hidehandRight} />
-        <div className={styles.hidehandLeft} />
-        <div className={styles.hideMouth} />
-        <div className={styles.cheekRight} />
-        <div className={styles.cheekLeft} />
-        <div className={styles.eyeRight}>
-          <div className={styles.eyeBlue} />
-          <div className={styles.eyeRed} />
-          <div className={styles.eyeWhite} />
-        </div>
-        <div className={styles.eyeLeft}>
-          <div className={styles.eyeBlue} />
-          <div className={styles.eyeRed} />
-          <div className={styles.eyeWhite} />
+      <div className={styles.all}>
+        <div className={styles.handRight} />
+        <div className={styles.handLeft} />
+        <div className={styles.footRight} />
+        <div className={styles.footLeft} />
+        <div className={styles.circleBody}>
+          <div className={styles.mouth} />
+          <div className={styles.hidehandRight} />
+          <div className={styles.hidehandLeft} />
+          <div className={styles.hideMouth} />
+          <div className={styles.cheekRight} />
+          <div className={styles.cheekLeft} />
+          <div className={styles.eyeRight}>
+            <div className={styles.eyeBlue} />
+            <div className={styles.eyeRed} />
+            <div className={styles.eyeWhite} />
+          </div>
+          <div className={styles.eyeLeft}>
+            <div className={styles.eyeBlue} />
+            <div className={styles.eyeRed} />
+            <div className={styles.eyeWhite} />
+          </div>
         </div>
       </div>
     </div>

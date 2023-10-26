046eef413ce3a9113d6dfe795eef1d12b41860f8
s1f102103796 <85663261+s1f102103796@users.noreply.github.com>
2023-10-26 17:09:49 +0900

    kirbyが歩くwalkButtonを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,94 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 import jumpStyles from './jump.module.css';
+import walkStyles from './walk.module.css';
 
 const Home = () => {
   const [isJumping, setIsJumping] = useState(false);
-  const handleButtonClick = () => {
+  const [isWalking, setIsWalking] = useState(false);
+
+  const JumpButtonClick = () => {
     setIsJumping(!isJumping);
+    setIsWalking(false);
+  };
+  const WalkButtonClick = () => {
+    setIsWalking(!isWalking);
+    setIsJumping(false);
   };
 
   return (
     <div className={styles.basePoint}>
       <div className={styles.background} />
-      <div className={isJumping ? `${styles.allMove} ${jumpStyles['allMove']}` : styles.allMove}>
-        {/* <div className={styles.allMove}> */}
-        <div className={styles.handRight} />
-        <div
-          className={isJumping ? `${styles.handLeft} ${jumpStyles['handLeft']}` : styles.handLeft}
-        />
-        {/* <div className={styles.handLeft} /> */}
+      <div
+        className={
+          isJumping && !isWalking
+            ? `${styles.allMove} ${jumpStyles['allMove']}`
+            : isWalking
+            ? `${styles.allMove} ${walkStyles['allMove']}`
+            : styles.allMove
+        }
+      >
         <div
           className={
-            isJumping ? `${styles.footRight} ${jumpStyles['footRight']}` : styles.footRight
+            isJumping && !isWalking
+              ? `${styles.handRight} ${jumpStyles['handRight']}`
+              : isWalking
+              ? `${styles.handRight} ${walkStyles['handRight']}`
+              : styles.handRight
           }
         />
-        {/* <div className={styles.footRight} /> */}
         <div
-          className={isJumping ? `${styles.footLeft} ${jumpStyles['footLeft']}` : styles.footLeft}
+          className={
+            isJumping && !isWalking
+              ? `${styles.handLeft} ${jumpStyles['handLeft']}`
+              : isWalking
+              ? `${styles.handLeft} ${walkStyles['handLeft']}`
+              : styles.handLeft
+          }
         />
-        {/* <div className={styles.footLeft} /> */}
+
+        <div
+          className={
+            isJumping && !isWalking
+              ? `${styles.footRight} ${jumpStyles['footRight']}`
+              : isWalking
+              ? `${styles.footRight} ${walkStyles['footRight']}`
+              : styles.footRight
+          }
+        />
+
+        <div
+          className={
+            isJumping && !isWalking
+              ? `${styles.footLeft} ${jumpStyles['footLeft']}`
+              : isWalking
+              ? `${styles.footLeft} ${walkStyles['footLeft']}`
+              : styles.footLeft
+          }
+        />
+
         <div className={styles.circleBody}>
           <div className={styles.mouth} />
-          <div className={styles.hidehandRight} />
-          {/* <div className={styles.hidehandLeft} /> */}
+          <div
+            className={
+              isJumping && !isWalking
+                ? `${styles.hidehandRight} ${jumpStyles['hidehandRight']}`
+                : isWalking
+                ? `${styles.hidehandRight} ${walkStyles['hidehandRight']}`
+                : styles.hidehandRight
+            }
+          />
+          <div
+            className={
+              isJumping && !isWalking
+                ? `${styles.hidehandLeft} ${jumpStyles['hidehandLeft']}`
+                : isWalking
+                ? `${styles.hidehandLeft} ${walkStyles['hidehandLeft']}`
+                : styles.hidehandLeft
+            }
+          />
           <div className={styles.hideMouth} />
-          {/* <div className={styles.hideCircle} /> */}
+
           <div className={styles.cheekRight} />
           <div className={styles.cheekLeft} />
           <div className={styles.eyeRight}>
@@ -49,8 +104,11 @@ const Home = () => {
         </div>
       </div>
 
-      <button className={styles.jumpButton} onClick={handleButtonClick}>
+      <button className={styles.jumpButton} onClick={JumpButtonClick}>
         Jump!
+      </button>
+      <button className={styles.walkButton} onClick={WalkButtonClick}>
+        Walk!
       </button>
     </div>
   );
